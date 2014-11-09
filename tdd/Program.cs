b359811dee406c7192dee28cba32eabc8fb7f100
scklepova <sckleppi@gmail.com>
2014-11-09 22:41:18 +0500

    new idea with stacks

--- tdd/Program.cs
+++ tdd/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,111 @@ namespace tdd
 {
     class Processor
     {
+
+        static Stack<string> TextBetweenTags;
+        static Stack<string> Tags;
+        static Stack<string> TagsInInput;
+        static string buffer;
+        static int index;
+
+        
+
+        public static string Rewrite(string inputText)
+        {
+            TextBetweenTags = new Stack<string>();
+            Tags = new Stack<string>();
+            TagsInInput = new Stack<string>();
+            index = 0;
+            buffer = "";
+
+            while (index < inputText.Length)
+            {
+                KeyValuePair<string, string> tag = BeginsWithTag(inputText);
+                string TagString = tag.Key;
+                string TagName = tag.Value;
+                if (TagName.Length > 0)
+                {
+                    PushTagInStack(TagString, TagName);
+                }
+                else
+                {
+                    buffer += inputText[index];
+                    index++;
+                }
+            }
+
+
+            string htmlCode = buffer;
+
+            while (TagsInInput.Any())
+            {
+                htmlCode = TagsInInput.Peek() + htmlCode + TextBetweenTags.Peek();
+                TagsInInput.Pop();
+                TextBetweenTags.Pop();
+            }
+
+            return htmlCode;
+        }
+
+
+
+
+        public static KeyValuePair<string, string> BeginsWithTag(string str)
+        {
+            return BeginWithSingleUnderscore(str);
+        }
+
+        public static KeyValuePair<string, string> BeginWithSingleUnderscore(string str)
+        {
+
+            if (index > 0 && IsDigitOrLetter(str[index - 1]) && index < str.Length - 1 && IsDigitOrLetter(str[index + 1]))
+                return EmptyPair();
+
+            if (str[index] != '_')
+                return EmptyPair();
+
+            string tagString = "_";
+            string tagName = "em";
+            return new KeyValuePair<string, string>(tagString, tagName);
+        }
+
+
+
+
+        public static bool SpaceSymbol(char c)
+        {
+            return (c == '\n' || c == '\t' || c == '\r' || c == ' ');
+        }
+
+
+
+
+        public static KeyValuePair<string, string> EmptyPair()
+        {
+            return new KeyValuePair<string, string>("", "");
+        }
+
+
+        public static void PushTagInStack(string tagString, string tagName)
+        {
+           
+
+            if (Tags.Any() && Tags.Peek() == tagName)
+            {
+                buffer = "<" + tagName + ">" + buffer + "</" + tagName + ">";
+                Tags.Pop();
+                TagsInInput.Pop();
+            }
+            else
+            {
+                TextBetweenTags.Push(buffer);
+                buffer = "";
+                TagsInInput.Push(tagString);
+                Tags.Push(tagName);
+            }
+            index += tagString.Length;
+        }
+
         public static bool IsDigitOrLetter(char c)
         {
             return (c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
@@ -19,37 +125,7 @@ namespace tdd
             if (index == 0 || index == str.Length - 1)
                 return false;
             return (str[index] == '_' && IsDigitOrLetter(str[index - 1]) && IsDigitOrLetter(str[index + 1]));
-                
-        }
 
-        public static string Rewrite(string inputText)
-        {
-            var i = 0;
-            bool underscoreRead = false;
-            string result = "";
-            while (i < inputText.Length)
-            {
-                if (inputText[i] == '_' && !IsUnderscoreBetweenDigitsOrLetters(inputText, i))
-                {
-                    if (underscoreRead)
-                        result += "</em>";
-                    else
-                        result += "<em>";
-
-                    underscoreRead = !underscoreRead;
-                    
-
-                }
-                else
-                {
-                    result += inputText[i];
-                    
-                }
-                i++;
-
-            }
-
-            return result;
         }
 
         static void Main(string[] args)
@@ -72,6 +148,13 @@ namespace tdd
         {
             var result = Processor.Rewrite("abc_def");
             Assert.AreEqual("abc_def", result);
+        }
+
+        [Test]
+        public void place_text_between_double_underscores_in_strong_tag()
+        {
+            var result = Processor.Rewrite("__bold__");
+            Assert.AreEqual("<strong>bold</strong>", result);
         }
     }
      
